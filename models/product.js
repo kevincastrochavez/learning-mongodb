748ee7b0caacfd5e55dccf32fc4c94cae06cfc96
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-25 13:49:58 -0600

    created connection database

--- models/product.js
+++ models/product.js
@@ -1,27 +1,38 @@
-// const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb;
 
-// const sequelize = require('../util/database');
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = title;
+  }
 
-// const Product = sequelize.define('product', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   }
-// });
+  save() {}
+}
 
-// module.exports = Product;
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -2,15 +2,30 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 require('dotenv').config();
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(process.env.MONGO_URI)
     .then((client) => {
       console.log('Connected');
+      _db = client.db();
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw 'No database found';
+};
+
+module.exports = {
+  mongoConnect,
+  getDb,
+};
