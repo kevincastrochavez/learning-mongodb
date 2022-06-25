5f0ca62d99aedda3997789daf5d8e2f54315086d
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-25 15:33:04 -0600

    modified functions to be able to edit and update a product

--- models/product.js
+++ models/product.js
@@ -2,22 +2,29 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
-    this.imageUrl = title;
+    this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
+    let dbOp;
 
-    return db
-      .collection('products')
-      .insertOne(this)
-      .then((result) => {
-        console.log(result);
-      });
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+
+    return dbOp.then((result) => {
+      console.log(result);
+    });
   }
 
   static fetchAll() {
